72b774f3164e78a8d0d3671d8eecdff494811d45
Dana Sevcik <danasevcik@hotmail.com>
2019-04-23 15:54:13 -0400

    import redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
