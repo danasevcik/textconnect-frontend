647fa0bb4786122e94f4c3a674fee55e7e724dce
Dana Sevcik <danasevcik@hotmail.com>
2019-05-01 13:49:56 -0400

    search feature for chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -5,13 +5,34 @@ import { updateConvo } from '../actions'
 
 class Chats extends Component {
 
+  state = {
+    searchTerm: ""
+  }
+
+  changeHandler = e => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+
   render() {
     // map through conversations and render ChatSliver for each
+    // check if the search term is included in username
     let conversations = this.props.conversations.map(conversation => {
-      return <ChatSliver key={conversation.id} conversation={conversation}/>
+      if (conversation.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())) {
+        return <ChatSliver key={conversation.id} conversation={conversation}/>
+      }
     })
     return (
       <div>
+        <div>
+          <h1>SearchChats</h1>
+          <input
+            type="text"
+            placeholder="search"
+            name="searchTerm"
+            value={this.state.searchTerm}
+            onChange={this.changeHandler}
+            />
+          </div>
         {conversations}
       </div>
     )
